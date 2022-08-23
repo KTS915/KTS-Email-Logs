4e5956c0c1516b51dd5ef18d3ed3259cc462411b
Tim Kaye <KTS915@users.noreply.github.com>
2022-08-23 17:50:41 -0400

    Update settings.php

--- inc/settings.php
+++ inc/settings.php
@@ -121,8 +121,8 @@ function kts_email_logs_storage_render() {
 
 	# Set default storage time to one week
 	$storage_options = array( 604800, 1209600, 1814400, 2419200, 15780000 );
-	if ( empty( $email_logs ) || empty( $email_logs['storage'] ) || ! in_array( $email_logs['storage'], $storage_options ) ) {
-		$email_logs['storage'] = 604800;
+	if ( empty( $email_logs ) || empty( $email_logs['storage'] ) || ! in_array( $email_logs['timescale'], $storage_options ) ) {
+		$email_logs['timescale'] = 604800;
 	} ?>
 
 	<fieldset>
@@ -134,24 +134,25 @@ function kts_email_logs_storage_render() {
 
 	<fieldset>
 		<label>
-			<input type="radio" name="email-logs[storage]" value="1" <?php checked( in_array( $email_logs['storage'], $storage_options ) ); ?>>
+			<input type="radio" name="email-logs[storage]" value="1" <?php checked( $email_logs['storage'], 1 ); ?>>
 			<span class="date-time-text date-time-custom-text"><?php _e( 'Yes: delete messages that are over', 'kts_email_logs' ); ?><span>
 
-				<select name="timescale">
+				<select id="timescale" name="email-logs[timescale]">
 
-					<option value="604800" <?php selected( $email_logs['storage'], 604800 ); ?>><?php _e( '1 week', 'kts_email_logs' ); ?></option>
+					<option value="604800" <?php selected( $email_logs['timescale'], 604800 ); ?>><?php _e( '1 week', 'kts_email_logs' ); ?></option>
 
-					<option value="1209600" <?php selected( $email_logs['storage'], 1209600 ); ?>><?php _e( '2 weeks', 'kts_email_logs' ); ?></option>
+					<option value="1209600" <?php selected( $email_logs['timescale'], 1209600 ); ?>><?php _e( '2 weeks', 'kts_email_logs' ); ?></option>
 
-					<option value="1814400" <?php selected( $email_logs['storage'], 1814400 ); ?>><?php _e( '3 weeks', 'kts_email_logs' ); ?></option>
+					<option value="1814400" <?php selected( $email_logs['timescale'], 1814400 ); ?>><?php _e( '3 weeks', 'kts_email_logs' ); ?></option>
 
-					<option value="2419200" <?php selected( $email_logs['storage'], 2419200 ); ?>><?php _e( '4 weeks', 'kts_email_logs' ); ?></option>
+					<option value="2419200" <?php selected( $email_logs['timescale'], 2419200 ); ?>><?php _e( '4 weeks', 'kts_email_logs' ); ?></option>
 
-					<option value="15780000" <?php selected( $email_logs['storage'], 15780000 ); ?>><?php _e( '6 months', 'kts_email_logs' ); ?></option>
+					<option value="15780000" <?php selected( $email_logs['timescale'], 15780000 ); ?>><?php _e( '6 months', 'kts_email_logs' ); ?></option>
 
 				</select>
 
 			</span><?php _e( ' old', 'kts_email_logs' ); ?></span>
+
 		</label>
 	</fieldset> <?php
 }
